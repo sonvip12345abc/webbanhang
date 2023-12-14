fd1fc6f8f16998b55622445b1b14a4c7bac04ae2
Rain <101036390+sonvip12345abc@users.noreply.github.com>
2023-12-14 22:24:53 +0700

    Update

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,102 +1,103 @@
-const mongoose =require('mongoose')
+const mongoose = require('mongoose')
 
-const orderSchema=mongoose.Schema({
-    shippingInfo:{
-        address:{
-            type:String,
-            required:true
+const orderSchema = mongoose.Schema({
+    shippingInfo: {
+        address: {
+            type: String,
+            required: true
         },
-        city:{
-            type:String,
-            required:true
+        city: {
+            type: String,
+            required: true
         },
-        phonrNo:{
-            type:String,
-            required:true
+        phoneNo: {
+            type: String,
+            required: true
         },
-        postalCode:{
-            type:String,
-            required:true
+        postalCode: {
+            type: String,
+            required: true
         },
-        country:{
-            type:String,
-            required:true
+        country: {
+            type: String,
+            required: true
         }
     },
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        required:true,
-        ref:'User'
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     },
-    orderItems:[
+    orderItems: [
         {
-            name:{
-                type:String,
-                required:true
+            name: {
+                type: String,
+                required: true
             },
-            name:{
-                type:Number,
-                required:true
-            }, 
-            image:{
-                type:String,
-                required:true
-            }, 
-            price:{
-                type:Number,
-                required:true
+            quantity: {
+                type: Number,
+                required: true
             },
-            product:{
-                type:mongoose.Schema.Types.ObjectId,
-                required:true,
-                ref:'Product'
+            image: {
+                type: String,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            },
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: 'Product'
             }
         }
     ],
-    paymentInfo:{
-        id:{
-            type:String
+    paymentInfo: {
+        id: {
+            type: String
         },
-        status:{
-            type:String
+        status: {
+            type: String
         }
     },
-    paidAt:{
-        type:Date
+    paidAt: {
+        type: Date
     },
-    itemsPrice:{
-        type:Number,
-        required:true,
-        default:0.0
+
+    itemsPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
     },
-    taxPrice:{
-        type:Number,
-        required:true,
-        default:0.0
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
     },
-    shippingPrice:{
-        type:Number,
-        required:true,
-        default:0.0
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
     },
-    totalPrice:{
-        type:Number,
-        required:true,
-        default:0.0
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
     },
-    orderStatus:{
-        type:String,
-        required:true,
-        default:'Processing'
+    orderStatus: {
+        type: String,
+        required: true,
+        default: 'Processing'
     },
-    deliverdAt:{
-        type:Date
+    deliveredAt: {
+        type: Date
     },
-    creatAt:{
-        type:Date,
-        default:Date.now
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
 
 })
 
-module.exports=mongoose.model('Order',orderSchema)
+module.exports = mongoose.model('Order', orderSchema)
