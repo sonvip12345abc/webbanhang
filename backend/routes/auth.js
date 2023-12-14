b5ce4cec586a89911007a791f7984f8760e11643
Rain <101036390+sonvip12345abc@users.noreply.github.com>
2023-12-14 22:25:42 +0700

    Update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,41 @@
-const express=require('express');
+const express = require('express');
+const router = express.Router();
 
-const router=express.Router();
-
-const{
-    registerUser, 
+const {
+    registerUser,
     loginUser,
-    logout, 
-    forgotPassword, 
+    forgotPassword,
     resetPassword,
     getUserProfile,
-    updatePassword
-}=require('../controllers/authController');
+    updatePassword,
+    updateProfile,
+    logout,
+    allUsers,
+    getUserDetails,
+    updateUser,
+    deleteUser
 
-const{isAuthenticateUSer}=require('../middlewares/auth')
+} = require('../controllers/authController');
+
+
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+
+router.route('/password/forgot').post(forgotPassword)
+router.route('/password/reset/:token').put(resetPassword)
+
 router.route('/logout').get(logout);
-router.route('/password/forgot').post(forgotPassword);
-router.route('/password/reset/:token').put(resetPassword);
-router.route('/me').get(isAuthenticateUSer,getUserProfile);
-router.route('/password/update').put(isAuthenticateUSer,updatePassword);
 
+router.route('/me').get(isAuthenticatedUser, getUserProfile)
+router.route('/password/update').put(isAuthenticatedUser, updatePassword)
+router.route('/me/update').put(isAuthenticatedUser, updateProfile)
 
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers)
+router.route('/admin/user/:id')
+    .get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateUser)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteUser)
 
-module.exports=router;
+module.exports = router;
