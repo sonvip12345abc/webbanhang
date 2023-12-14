932ef96bc8d241b985feefaf4a635ed586c51921
Lê Thái <thaileducylvpvn@gmail.com>
2023-12-15 01:45:22 +0700

    Update

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -11,4 +11,4 @@ const { isAuthenticatedUser } = require('../middlewares/auth')
 router.route('/payment/process').post(isAuthenticatedUser, processPayment);
 router.route('/stripeapi').get(isAuthenticatedUser, sendStripApi);
 
-module.exports = router;
+module.exports = router
