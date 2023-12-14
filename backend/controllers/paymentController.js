f06c9f41027cd0974b57ef4654d43700de46a05e
Lê Thái <thaileducylvpvn@gmail.com>
2023-12-15 01:47:33 +0700

    Update

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,10 +1,9 @@
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 // Process stripe payments   =>   /api/v1/payment/process
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
-
     const paymentIntent = await stripe.paymentIntents.create({
         amount: req.body.amount,
         currency: 'usd',
@@ -16,7 +15,6 @@ exports.processPayment = catchAsyncErrors(async (req, res, next) => {
         success: true,
         client_secret: paymentIntent.client_secret
     })
-
 })
 
 
@@ -26,5 +24,4 @@ exports.sendStripApi = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({
         stripeApiKey: process.env.STRIPE_API_KEY
     })
-
 })
