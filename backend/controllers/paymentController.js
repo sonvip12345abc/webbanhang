73b1d9fbeab598b4d898636d069eb3b08cf21df3
Lê Thái <thaileducylvpvn@gmail.com>
2023-12-20 10:58:59 +0700

    Update controller

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -2,7 +2,7 @@ const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
-// Process stripe payments   =>   /api/v1/payment/process
+// Process Stripe payments   =>   /api/v1/payment/process
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
     const paymentIntent = await stripe.paymentIntents.create({
         amount: req.body.amount,
@@ -18,7 +18,7 @@ exports.processPayment = catchAsyncErrors(async (req, res, next) => {
 })
 
 
-// Send stripe API Key   =>   /api/v1/stripeapi
+// Send stripe_API_Key   =>   /api/v1/stripeapi
 exports.sendStripApi = catchAsyncErrors(async (req, res, next) => {
 
     res.status(200).json({
