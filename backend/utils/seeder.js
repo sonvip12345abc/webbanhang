5db4de6e336fb139aebfd2ed7673d5014ece64a6
Lê Thái <thaileducylvpvn@gmail.com>
2023-12-21 09:58:27 +0700

    Update

--- backend/utils/seeder.js
+++ backend/utils/seeder.js
@@ -11,12 +11,11 @@ connectDatabase();
 
 const seedProducts = async () => {
     try {
-
         await Product.deleteMany();
         console.log('Products are deleted');
 
         await Product.insertMany(products)
-        console.log('All Products are added.')
+        console.log('All Products are added');
 
         process.exit();
 
