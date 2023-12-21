7bdcd9f502a856dd736c505b279eacada22903e8
Rain <101036390+sonvip12345abc@users.noreply.github.com>
2023-12-21 22:19:39 +0700

    Update

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -14,7 +14,12 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    r
+    req.user = await User.findById(decoded.id);
+
+    next()
+})
+
+// Handling users roles
 exports.authorizeRoles = (...roles) => {
     return (req, res, next) => {
         if (!roles.includes(req.user.role)) {
