158bfa62835023fb45c6fc5d290ae35ab6b415b8
Rain <101036390+sonvip12345abc@users.noreply.github.com>
2023-12-21 22:26:45 +0700

    Update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -23,16 +23,16 @@ const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 
-router.route('/password/forgot').post(forgotPassword)
-router.route('/password/reset/:token').put(resetPassword)
+router.route('/password/forgot').post(forgotPassword);
+router.route('/password/reset/:token').put(resetPassword);
 
 router.route('/logout').get(logout);
 
-router.route('/me').get(isAuthenticatedUser, getUserProfile)
-router.route('/password/update').put(isAuthenticatedUser, updatePassword)
-router.route('/me/update').put(isAuthenticatedUser, updateProfile)
+router.route('/me').get(isAuthenticatedUser, getUserProfile);
+router.route('/password/update').put(isAuthenticatedUser, updatePassword);
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
 
-router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers)
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
 router.route('/admin/user/:id')
     .get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
     .put(isAuthenticatedUser, authorizeRoles('admin'), updateUser)
