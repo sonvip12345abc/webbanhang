5ec426276759bb0455833e560a25f5e6fdd363d9
Rain <101036390+sonvip12345abc@users.noreply.github.com>
2023-12-21 22:21:49 +0700

    Update

--- backend/models/order.js
+++ backend/models/order.js
@@ -12,10 +12,40 @@ const orderSchema = mongoose.Schema({
         },
         phoneNo: {
             type: String,
-        
+            required: true
+        },
+        postalCode: {
+            type: String,
+            required: true
+        },
+        country: {
+            type: String,
+            required: true
+        }
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    orderItems: [
+        {
+            name: {
+                type: String,
                 required: true
             },
-            quantity
+            quantity: {
+                type: Number,
+                required: true
+            },
+            image: {
+                type: String,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            },
             product: {
                 type: mongoose.Schema.Types.ObjectId,
                 required: true,
