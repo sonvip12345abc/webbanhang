38d992dfe5dfe8eb36fd6a4cabe78fccc9f92eca
Lê Thái <thaileducylvpvn@gmail.com>
2023-12-22 14:51:20 +0700

    Update

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -12,8 +12,8 @@ class APIFeatures {
             }
         } : {}
 
-        this.query = this.query.find({ ...keyword });
-        return this;
+        this.query = this.query.find({ ...keyword })
+        return this
     }
 
     filter() {
@@ -27,8 +27,8 @@ class APIFeatures {
         let queryStr = JSON.stringify(queryCopy);
         queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`);
 
-        this.query = this.query.find(JSON.parse(queryStr));
-        return this;
+        this.query = this.query.find(JSON.parse(queryStr))
+        return this
     }
 
     pagination(resPerPage) {
