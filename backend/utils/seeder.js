c566a056bce88a16fa75b9e515a0adcb2f64ba3a
Lê Thái <thaileducylvpvn@gmail.com>
2023-12-24 12:59:14 +0700

    Update

--- backend/utils/seeder.js
+++ backend/utils/seeder.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 const dotenv = require('dotenv');
-const connectDatabase = require('../config/database')
-const products = require('../data/products')
+const connectDatabase = require('../config/database');
+const products = require('../data/products');
 
 // Setting dotenv file
 dotenv.config({ path: 'backend/config/config.env' })
