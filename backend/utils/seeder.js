823a1707c3e8ffbca0435c47d1f5d96723d75253
Lê Thái <thaileducylvpvn@gmail.com>
2023-12-25 09:56:47 +0700

    Update

--- backend/utils/seeder.js
+++ backend/utils/seeder.js
@@ -1,5 +1,5 @@
-const Product = require('../models/product');
-const dotenv = require('dotenv');
+const Product = require('../models/product')
+const dotenv = require('dotenv')
 const connectDatabase = require('../config/database');
 const products = require('../data/products');
 
